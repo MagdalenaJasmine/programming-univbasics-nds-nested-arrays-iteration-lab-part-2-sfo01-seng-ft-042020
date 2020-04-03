58e8018022c33302e17c0a0a3f1d1eb6ea6c9260
Magdalena Cooper <magdalenacooper92@gmail.com>
2020-04-03 15:12:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_min_in_nested_arrays(src)
 outer_results = []
 row_index = 0
   while row_index < src.count do
-    element_index = 0 
+    element_index = 0
     minimum_string_element = ""
     while element_index < src[row_index].count do 
       if src.min < minimum_string_element
