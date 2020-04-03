2161e487881c08e8f1c482ef08af20f33c95a69b
Magdalena Cooper <magdalenacooper92@gmail.com>
2020-04-03 15:58:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ results = []
 row_index = 0
   while row_index < src.count do
     element_index = 0
-    minimum_array_element = 0
+    minimum_array_element = src[0]
     while element_index < src[row_index].count do 
       if src[row_index][element_index] < minimum_array_element
         minimum_array_element = src[row_index][element_index]
